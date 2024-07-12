09c6be983d4b2933b1b7716164d06e573ac7c872
LamiaAsadkhan <lamiaasadkhan@gmail.com>
2024-07-12 10:04:44 -0700

    .

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,19 @@
-function formSubmit() {
-    alert("Form submitted!");
+// Function to display the clicked painting in a modal
+function showPainting(imageName) {
+    var modal = document.getElementById("paintingModal");
+    var modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = "{{ url_for('static', filename='images/') }}" + imageName;
 }
+
+function closeModal() {
+    var modal = document.getElementById("paintingModal");
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("paintingModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
